d56047f68ef60e6aea1e1eadbe13b2b852a334af
Martín García <88008897+Naufrxgo@users.noreply.github.com>
2021-11-29 21:21:44 -0600

    Add files via upload

---
+++ carusel.js
@@ -0,0 +1,53 @@
+$(function(){
+	$('.center').slick({
+		infinite:true,
+		slidesToShow: 4,
+    slidesToScroll: 1,
+		arrows:false,
+		autoplay: true,
+		autoplaySpeed: 3900,
+		dots: false,
+		centerModel:true,
+        centerPadding:'60px',
+        responsive: [
+            {
+              breakpoint: 1024,
+              settings: {
+                slidesToShow: 3,
+                slidesToScroll: 1,
+                infinite: true,
+                dots: true
+              }
+            },
+            {
+              breakpoint: 600,
+              settings: {
+                slidesToShow: 2,
+                slidesToScroll: 2
+              }
+            },
+            {
+              breakpoint: 480,
+              settings: {
+                slidesToShow: 1,
+                slidesToScroll: 1
+              }
+            }
+          ]
+	});
+});
+
+const onProgress = (event) => {
+  const progressBar = event.target.querySelector('.progress-bar');
+  const updatingBar = event.target.querySelector('.update-bar');
+  updatingBar.style.width = `${event.detail.totalProgress * 100}%`;
+  if (event.detail.totalProgress === 1) {
+    progressBar.classList.add('hide');
+  } else {
+    progressBar.classList.remove('hide');
+    if (event.detail.totalProgress === 0) {
+      event.target.querySelector('.center-pre-prompt').classList.add('hide');
+    }
+  }
+};
+document.querySelector('model-viewer').addEventListener('progress', onProgress);
